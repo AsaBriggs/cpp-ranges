5bd3e9f913c9f14b8fe00083a495ad17d6fc8983
Asa Briggs <asabriggs@fastmail.fm>
2014-07-01 11:56:37 +0100

    Initial Commit

---
+++ algo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algo
+//
+//  Created by Asa Briggs on 01/07/2014.
+//  Copyright (c) 2014 Asa Briggs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

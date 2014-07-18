9c2f57556a32c746e6bfbf113aef25c22defc811
Asa Briggs <asabriggs@fastmail.fm>
2014-07-18 08:50:29 +0100

    Delete unused files in anticipation of a rename

--- algo/algo_template_params.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  algo_template_params.cpp
-//  algo
-//
-//  Created by Asa Briggs on 05/07/2014.
-//  Copyright (c) 2014 Asa Briggs. All rights reserved.
-//
-
-#include "algo_template_params.h"
